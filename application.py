6c8fc895c2dbd8ba2b1cc08a151dcc9eb6fa8bb8
MadhuriHB <madhurisubhash2002@gmail.com>
2016-07-23 17:02:39 -0700

    Removed the default values

--- application.py
+++ application.py
@@ -29,10 +29,10 @@ def fill_form():
 @app.route("/application", methods=["POST"])
 def process_application():
     """Show the response message with details filled in. """
-    first_name = request.form.get("firstname", "mad")
-    last_name = request.form.get("lastname", "pat")
-    salary = request.form.get("salary", "100")
-    job = request.form.get("select", "wife")
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+    salary = request.form.get("salary")
+    job = request.form.get("select")
     return render_template("application-response.html", firstname=first_name,
                             lastname=last_name, salary=salary, jobskill=job)
 
